914634a0e73ad626a6a61dc41846872c4b7255ae
bo.han <bo.han@amlogic.com>
2024-03-01 14:31:03 +0800

    daily study

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.options = {
+  -- 指定使用的NvChad分支为v2.0
   nvchad_branch = "v2.0",
 }
 
@@ -19,6 +20,7 @@ M.ui = {
   extended_integrations = {}, -- these aren't compiled by default, ex: "alpha", "notify"
 
   -- cmp themeing
+  -- 配置完成菜单插件
   cmp = {
     icons = true,
     lspkind_text = true,
@@ -27,9 +29,11 @@ M.ui = {
     selected_item_bg = "colored", -- colored / simple
   },
 
+  -- 配置文件搜索插件
   telescope = { style = "borderless" }, -- borderless / bordered
 
   ------------------------------- nvchad_ui modules -----------------------------
+  -- 配置状态栏主题、分隔符和覆盖模块
   statusline = {
     theme = "default", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
@@ -39,6 +43,7 @@ M.ui = {
   },
 
   -- lazyload it when there are 1+ buffers
+  -- 配置标签栏行为，显示数字、延迟加载和覆盖模块
   tabufline = {
     show_numbers = false,
     enabled = true,
@@ -47,6 +52,7 @@ M.ui = {
   },
 
   -- nvdash (dashboard)
+  -- 配置内置仪表板，包括其标题、按钮和加载行为。
   nvdash = {
     load_on_startup = false,
 
@@ -72,6 +78,7 @@ M.ui = {
     },
   },
 
+  -- 设置内置秘籍的主题
   cheatsheet = { theme = "grid" }, -- simple/grid
 
   lsp = {
@@ -83,10 +90,13 @@ M.ui = {
   },
 }
 
+-- 定义自定义插件配置文件的路径
 M.plugins = "" -- path i.e "custom.plugins", so make custom/plugins.lua file
 
+--加载lazy.nvim插件管理器配置
 M.lazy_nvim = require "plugins.configs.lazy_nvim" -- config for lazy.nvim startup options
 
+-- 加载键盘映射配置
 M.mappings = require "core.mappings"
 
 return M
